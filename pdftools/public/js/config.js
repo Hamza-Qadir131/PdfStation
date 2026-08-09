2e41a42fa87df374bca6ce4cbd8c15df1e533704
Hamza-Qadir131 <hamzaqadir02@gmail.com>
2026-08-09 13:37:38 -0700

    link add

--- pdftools/public/js/config.js
+++ pdftools/public/js/config.js
@@ -1,3 +1,3 @@
 // Change this one line when you deploy the backend somewhere
 // other than your own machine (see README "Deploying" section).
-window.PDFSTATION_API_BASE = "https://pdfstation-production-b988.up.railway.app";
+window.PDFSTATION_API_BASE = "pdfstation-production-b988.up.railway.app";
